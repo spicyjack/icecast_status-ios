a9423a9002bc016619268d3e0d5d9b5b67e1f9ed
Brian Manning <brian@xaoc.org>
2012-05-12 10:39:16 -0700

    fixed call to incorrect method in IcecastStatusParser

--- IcecastStatus/ViewController.m
+++ IcecastStatus/ViewController.m
@@ -97,7 +97,7 @@ NSString *defaultURLString = @"http://stream.xaoc.org:7767/simple.xsl";
     NSLog(@"Creating IcecastStatusParser...");
     parser = [[IcecastStatusParser alloc] init];
     NSLog(@"Calling parser");
-    [parser fetchAndParseIcecastStatusPage:self withURL:url];
+    [parser doFetchAndParseIcecastStatusHTML:self withURL:url];
 }
 
 
